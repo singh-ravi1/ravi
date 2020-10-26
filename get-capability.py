252df12f129197480da4097aec1efa3e33a5aac0
singh-ravi1 <66844522+singh-ravi1@users.noreply.github.com>
2020-10-26 15:51:51 -0400

    Delete get-capability.py

--- get-capability.py
+++
@@ -1,8 +0,0 @@
-from ncclient import manager
-
-m = manager.connect(host="10.0.0.1", port=830, username="cisco", password="cisco", hostkey_verify=False)
-
-for capabilities in m.server_capabilities:
-	print(capabilities)	
-
-
